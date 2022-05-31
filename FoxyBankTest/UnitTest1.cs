e557a516e08dc0fc233c66f0520ae04f5f03392f
MattiasKo <91311332+MattiasKo@users.noreply.github.com>
2022-05-31 19:30:03 +0200

    test

--- FoxyBankTest/UnitTest1.cs
+++ FoxyBankTest/UnitTest1.cs
@@ -13,9 +13,8 @@ namespace FoxyBankTest
             User LoginUser = new User("Isak", "Jensen", "Hemlis123", 2001);
             //act
             var actual = LoginUser.Authentication(LoginUser.PassWord, LoginUser.UserId);
-            var expected = true;
             //assert
-            Assert.AreEqual(expected, actual);
+            Assert.IsTrue(actual);
         }
         [TestMethod]
         public void Bank_User_And_Bank_Matching()
@@ -63,8 +62,7 @@ namespace FoxyBankTest
             user.BankAccounts[2].AccountName = "Konto i Amerikanska dollar";
             user.BankAccounts[2].AddBalance(10000);
             //act 1
-            var actual1 = user.BankAccounts[0].AccountName;
-            var expected1 = "Personkonto";
+            string actual1 = user.BankAccounts[0].AccountName;
 
             user.BankAccounts[1].AddBalance(10000);
             var actual2 = user.BankAccounts[1].GetBalance();
@@ -79,12 +77,43 @@ namespace FoxyBankTest
             string actual4 = string.Format("{0:0.00}", user.BankAccounts[2].GetBalance());
             string expected4 = "11097,69";
             //assert
-            Assert.AreEqual(actual1, expected1);
-            Assert.AreEqual(actual2, expected2);
-            Assert.AreEqual(actual3, expected3);
-            Assert.AreEqual(actual4, expected4);
+
+            StringAssert.Contains(actual1, "Personkonto");
+            Assert.AreEqual(expected2, actual2);
+            Assert.AreEqual(expected3, actual3);
+            Assert.AreEqual(expected4, actual4);
+           
 
         }
-       
+        [TestMethod]
+        public void Bank_User_Take_Loan()
+        {
+            //arrange
+            Bank bank = new Bank();
+            User user = new User("Isak", "Jensen", "Hemlis123", 2001);
+            //user.BankAccounts.Add(new LoanAccount(10000));
+            LoanAccount Loan1 = new LoanAccount(10000);
+            user.BankAccounts.Add(Loan1);
+            bank.BankAccounts.Add(10000, 2001);
+            user.BankAccounts[0].AccountName = "Lånekonto";
+            user.BankAccounts[0].CurrencySign = "kr";
+            user.BankAccounts[0].AddBalance(-10000);
+           
+            Loan1.SetInterest(0.06M);
+
+            //act
+            var actual1 = Loan1.GetInterest();
+            var expected1 = 0.06M;
+
+            var actual2 = user.BankAccounts.Contains(Loan1);
+            var expected2 = user.BankAccounts[0].AccountNr.Equals(10000);
+
+            //assert
+            Assert.AreEqual(expected1, actual1);
+            Assert.AreEqual(expected2, actual2);
+
+        }
+
+
     }
 }
